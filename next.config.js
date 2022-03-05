5a199901f10a0a4262cd86d6012277d08ff2a4be
Mohammed alreai <mhamad.aa1997.aa@gmail.com>
2022-03-05 16:53:49 +0200

    finsh  simplae  app

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+
+  reactStrictMode: true,
+  images: {
+    domains: ['www-cdn.bigcommerce.com'],
+
+    deviceSizes: [320, 480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+}
