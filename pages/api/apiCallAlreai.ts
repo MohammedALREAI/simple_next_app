9f1e27898e12e8a78349c3bffa2ce9646977af51
MohammedALREAI <mhamad.aa1997.aa@gmail.com>
2022-10-02 15:49:42 +0300

    add apiRoute

---
+++ pages/api/apiCallAlreai.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type {NextApiRequest, NextApiResponse} from "next";
+import NextCors from "nextjs-cors";
+
+type Data = {
+  url: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | any>
+) {
+  try {
+    const {url} = req.body;
+
+    console.log("url=====>>>>>>>", url);
+    await NextCors(req, res, {
+      // Options
+      methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+      origin: "*",
+      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
+
+    console.log("url=====>>>>>>>", req.body);
+    //     const responseData = await fetch(`http://aiph.me:8000/api${url}`);
+
+    //     const dataToJson = await responseData.json();
+
+    res.status(200).json({dataToJson: []});
+  } catch (error) {
+    console.log("error", error);
+  }
+}
